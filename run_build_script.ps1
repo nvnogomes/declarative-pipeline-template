87fb419591c92f1870d5c2505bd6cdb69bc76762
nvno <nvnogomes@gmail.com>
2024-11-28 15:05:52 +0000

    Create run_build_script.ps1

---
+++ run_build_script.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env pwsh
+# Running build script for demo pipeline
+Write-Output "Running build script for demo pipeline"
+
+# Simulating the build process
+Write-Output "building demoapp"
+Write-Output "*"
+Write-Output "**"
+Write-Output "***"
+Write-Output "****"
+Write-Output "*****"
+
+# Build success message
+Write-Output "Build successful"
